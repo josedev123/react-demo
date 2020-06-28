fec0ba82735b9420344d8d58477b6b3bb4aac820
Jose <jose8055@gmail.com>
2020-06-28 19:31:03 -0400

    Class Components

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 class Employee extends React.Component{
   render(){
-    return <div>
+    return <div className="employeeDetails">
         <h2>Employee Details</h2>
         <p>
           <label>Employee ID : <b>{this.props.Id}</b></label>
@@ -16,10 +16,29 @@ class Employee extends React.Component{
           <label>Employee Location : <b>{this.props.Location}</b></label>
         </p>
 
+      <Department DeptName={this.props.DeptName} HeadName={this.props.DeptName}></Department>
     </div>
   }
 }
 
-const element =<Employee Id="2" Name="Employee Name" Location="Atlanta, GA" ></Employee>
+class Department extends React.Component{
+  render() {
+    return <div className="deptInfo">
+      <h2>Department Details</h2>
+
+      <p>
+          <label>Dept Name : <b>{this.props.DeptName}</b></label>
+        </p>
+
+        <p>
+          <label>Head Name : <b>{this.props.HeadName}</b></label>
+        </p>
+    </div>
+
+  }
+
+}
+
+const element =<Employee Id="2" Name="Employee Name" Location="Atlanta, GA" DeptName="dev" HeadName="Dept. Name"></Employee>
 
 ReactDOM.render(element, document.getElementById("root"));
