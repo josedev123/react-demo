0968dfed40063385edf0b313667e1bf24cfec1c0
Jose <jose8055@gmail.com>
2020-06-28 19:20:27 -0400

    Class Component - basic

--- src/index.js
+++ src/index.js
@@ -1,39 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
-// function DisplayEmployeeInfo(employee) {}
+class Employee extends React.Component{
+  render(){
+    return <div>
+        <h2>Employee Details</h2>
+        <p>
+          <label>Employee ID : <b>{this.props.Id}</b></label>
+        </p>
+        <p>
+          <label>Employee Name : <b>{this.props.Name}</b></label>
+        </p>
+        <p>
+          <label>Employee Location : <b>{this.props.Location}</b></label>
+        </p>
 
-var DisplayEmployeeInfo=(employee)=>{
-
-  return <div className="employeeDetails">
-    <h1>Employee Details ...</h1>
-    <p>
-    <label> Employee ID : <b>{employee.Id}</b></label>
-    </p>
-    <p>
-    <label> Employee Name : <b>{employee.Name}</b></label>
-    </p>
-    <p>
-    <label> Employee Location : <b>{employee.Location}</b></label>
-    </p>
-    <Department deptName={employee.deptName} headName={employee.headName}></Department>
-
-  </div>;
-
-  
-  };
-
-
-  const Department=(deptInfo)=>{
-    return <div className="deptInfo"><p>Dept Name is : <b>{deptInfo.deptName}</b></p>
-      <p>Dept Head is : <b>{deptInfo.headName}</b></p>
-      </div>;
+    </div>
   }
-  
-  const element=<DisplayEmployeeInfo Id="101" Name="test name" Location="Atlanta, GA" deptName="UI Dev" headName="React"></DisplayEmployeeInfo>
-  
+}
+
+const element =<Employee Id="2" Name="Employee Name" Location="Atlanta, GA" ></Employee>
 
 ReactDOM.render(element, document.getElementById("root"));
-
