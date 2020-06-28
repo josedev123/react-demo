581e3906734e34071668c9e00b045f99ee1a8638
Jose <jose8055@gmail.com>
2020-06-28 18:24:39 -0400

    Functional Components

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 
 var DisplayEmployeeInfo=(employee)=>{
 
-  return <div>
+  return <div className="employeeDetails">
     <h1>Employee Details ...</h1>
     <p>
     <label> Employee ID : <b>{employee.Id}</b></label>
@@ -18,13 +18,21 @@ var DisplayEmployeeInfo=(employee)=>{
     <p>
     <label> Employee Location : <b>{employee.Location}</b></label>
     </p>
-  
+    <Department deptName={employee.deptName} headName={employee.headName}></Department>
+
   </div>;
+
   
   };
+
+
+  const Department=(deptInfo)=>{
+    return <div className="deptInfo"><p>Dept Name is : <b>{deptInfo.deptName}</b></p>
+      <p>Dept Head is : <b>{deptInfo.headName}</b></p>
+      </div>;
+  }
   
-  
-  const element=<DisplayEmployeeInfo Id="101" Name="test name" Location="Atlanta, GA" ></DisplayEmployeeInfo>
+  const element=<DisplayEmployeeInfo Id="101" Name="test name" Location="Atlanta, GA" deptName="UI Dev" headName="React"></DisplayEmployeeInfo>
   
 
 ReactDOM.render(element, document.getElementById("root"));
